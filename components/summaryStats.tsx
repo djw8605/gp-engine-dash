d02c372ec377ef10c61fb51a0e235d9615b7aad6
Derek Weitzel <djw8605@gmail.com>
2024-01-09 11:49:05 -0600

    Adding spinning loading icon for summary stats

--- components/summaryStats.tsx
+++ components/summaryStats.tsx
@@ -2,7 +2,7 @@
 
 import useSWR from "swr";
 import { Site } from "../lib/states";
-import { ComputerDesktopIcon, CpuChipIcon, UserGroupIcon } from "@heroicons/react/24/outline";
+import { ArrowPathIcon, ComputerDesktopIcon, CpuChipIcon, UserGroupIcon } from "@heroicons/react/24/outline";
 
 /* 
 
@@ -21,11 +21,23 @@ function SummaryStat({ title, value, icon, bgColor }: { title: string, value: nu
   return (
     <>
       <div className={`${!bgColor ? "bg-slate-100" : ""} rounded-xl p-4 py-6 flex text-white w-full`}
-      style={{ backgroundColor: bgColor as string }}>
+        style={{ backgroundColor: bgColor as string }}>
         <div className="flex lg:gap-8 gap-4 items-center w-full">
           {icon && <span className='text-gray-200 h-[5em] w-[7em]'>{icon}</span>}
           <div className='flex flex-col gap-1 w-full'>
-            <div className='text-4xl font-bold w-full'>{value && Math.round(value).toLocaleString()}</div>
+            {value ? (
+              <div className='text-4xl font-bold w-full'>{value && Math.round(value).toLocaleString()}
+              </div>) :
+              (
+                <div className="flex gap-2 items-center">
+                  <div>
+                    <ArrowPathIcon className='animate-spin h-6 w-6' />
+                  </div>
+                  <div className='text-4xl font-bold'>Loading...</div>
+                </div>
+              )
+            }
+
             <div className='text-sm'>{title}</div>
           </div>
         </div>
@@ -56,8 +68,8 @@ export default function SummaryStats({ sites }: { sites: Site[] }) {
       <div className='text-xl font-bold py-2'>Summary Statistics of last 30 days</div>
       <div className="grid grid-flow-col gap-2 py-4">
         <SummaryStat title='Research Groups Enabled' value={data ? data.length : null} icon={<UserGroupIcon />} bgColor="#754668" />
-        <SummaryStat title='GPU Hours' value={totalGpuHours} icon={<ComputerDesktopIcon />} bgColor="#EE6C4D"/>
-        <SummaryStat title='CPU Hours' value={totalCpuHours} icon={<CpuChipIcon />} bgColor="#68805e"/>
+        <SummaryStat title='GPU Hours' value={totalGpuHours} icon={<ComputerDesktopIcon />} bgColor="#EE6C4D" />
+        <SummaryStat title='CPU Hours' value={totalCpuHours} icon={<CpuChipIcon />} bgColor="#68805e" />
       </div>
     </>
   )
