e0271cd1133dc019a75af02739b61f2b141c6d2d
Derek Weitzel <djw8605@gmail.com>
2024-06-06 12:47:17 -0500

    Fix initial downloading of summary stats

--- lib/downloadSummaryStats.ts
+++ lib/downloadSummaryStats.ts
@@ -3,7 +3,7 @@
 export default async function downloadSummaryStats() {
   // Just download and return the results from https://s3-west.nrp-nautilus.io/dweitzel/gp-engine/website/summary_stats.json
   // This is a static file that is updated every day
-  const url = 'https://s3-west.nrp-nautilus.io/dweitzel/gp-engine/website/summary_stats.json';
+  const url = 'https://api.gp-engine.org/gp-engine-dash/summary_stats.json';
   const response = await fetch(url);
   const data = await response.json();
   return data;
