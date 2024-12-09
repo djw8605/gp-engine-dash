af901137f7be6c946e1b91d548f9199b6188414f
Derek Weitzel <djw8605@gmail.com>
2024-12-09 13:19:25 -0600

    Update the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
       <>
         <footer className='flex bg-green-900 items-center justify-between flex-wrap p-6'>
           <div className="container mx-auto">
-          This project is supported by National Science Foundation under Grant <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2322218" className="underline">OAC-2322218</a>. Any opinions, findings, conclusions or recommendations expressed in this material are those of the authors and do not necessarily reflect the views of the National Science Foundation.
+          This project is supported by the National Science Foundation under Grant <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2322218" className="underline">OAC-2322218</a>. Any opinions, findings, conclusions or recommendations expressed in this material are those of the authors and do not necessarily reflect the views of the National Science Foundation.
           </div>
           <div className='container mx-auto grid grid-cols-4 gap-4'>
   
